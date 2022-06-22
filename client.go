4425d65fdd289c9c99ebab04d9ddfa4ff1a61f33
Yunus AYDIN <aydinnyunus@gmail.com>
2022-06-23 02:20:36 +0300

    Update client.go

--- client.go
+++ client.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	API_ROOT = "https://blockchain.info"
+	ETH_ROOT = "https://api.blockchain.info"
 )
 
 type Client struct {
@@ -38,6 +39,33 @@ func (c *Client) loadResponse(path string, i interface{}, formatJson bool) error
 	}
 
 	return json.Unmarshal(b, &i)
+}
+
+func (c *Client) loadETHResponse(path string, i interface{}, getSummary bool) error {
+	full_path := ETH_ROOT + path
+	if getSummary {
+		full_path = ETH_ROOT + path + "/summary"
+	} else{
+		full_path = ETH_ROOT + path + "/transactions?page=0&size=200"
+	}
+
+	fmt.Println("querying..." + full_path)
+	rsp, e := c.Get(full_path)
+	if e != nil {
+		return e
+	}
+
+	defer rsp.Body.Close()
+
+	b, e := ioutil.ReadAll(rsp.Body)
+	if e != nil {
+		return e
+	}
+	if rsp.Status[0] != '2' {
+		return fmt.Errorf("expected status 2xx, got %s: %s", rsp.Status, string(b))
+	}
+
+	return json.Unmarshal(b,&i)
 }
 
 func New() (*Client, error) {
